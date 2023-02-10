644b18f3d353c4ae76a3cac0b9b09dd09bfb411b
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-02-10 22:51:10 +0000

    Deploying to gh-pages from @ MelnykovSV/goit-js-hw-11@de62b21bd5a1af24efd30875d7060fc8b169185d 🚀

--- index.afb8ac69.js
+++
@@ -1,2 +0,0 @@
-fetch(`https://pixabay.com/api/?key=33543328-1e01a52b77697b8d064c91a7e&q=${"cat"}&image_type=photo&orientation=horizontal&safesearch=true`).then((t=>{if(!t.ok)throw new Error(t.status);return t.json()})).then((t=>{console.dir(t)}));
-//# sourceMappingURL=index.afb8ac69.js.map

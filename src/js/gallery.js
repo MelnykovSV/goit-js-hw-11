b2698c8aee6265230dcba21dd33c4c747b414d0e
MelnykovSV <svmelnykov@gmail.com>
2023-02-15 04:19:15 +0200

    Staple?

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,25 +1,18 @@
 import { fetchImages } from './fetchImages';
 import { renderGallery } from './renderGallery';
-import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-
-// import { lightboxGallery } from './renderGallery';
 
 let page = 1;
 let currentQuery;
 let totalPages;
-// let foundPicsNumber;
 
 const MAX_PAGES = 13;
 const throttle = require('lodash.throttle');
 export const gallery = document.querySelector('.gallery-content');
 const searchForm = document.querySelector('.search-form');
 const spinner = document.querySelector('.lds-roller');
-// const lightboxGallery = new SimpleLightbox('.gallery-content a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
 
 //Detects scrolling to the end of the page
 
@@ -94,53 +87,10 @@ searchForm.addEventListener('submit', async e => {
   }
 });
 
-//renders gallery
-
-// function renderGallery(arrayOfObjects) {
-//   const listArray = arrayOfObjects.map(item => {
-//     const listData = {};
-//     listData.webformatURL = item.webformatURL;
-//     listData.largeImageURL = item.largeImageURL;
-//     listData.alt = item.tags;
-//     listData.likes = item.likes;
-//     listData.views = item.views;
-//     listData.comments = item.comments;
-//     listData.downloads = item.downloads;
-//     return listData;
-//   });
-
-//   const markup = listArray
-//     .map(item => {
-//       return `<a href="${item.largeImageURL}" class="photo-card gallery-item" data-capture="${item.tags}"><div class="photo-card__thumb"><img src="${item.webformatURL}" alt="${item.alt}" loading="lazy" width="320" height="220" data-largeImageURL='${item.largeImageURL}'/></div>
-//       <div class="info">
-//         <p class="info-item">
-//           <b>Likes</b>
-//           ${item.likes}
-//         </p>
-//         <p class="info-item">
-//           <b>Views</b>
-//           ${item.views}
-//         </p>
-//         <p class="info-item">
-//           <b>Comments</b>
-//           ${item.comments}
-//         </p>
-//         <p class="info-item">
-//           <b>Downloads</b>
-//           ${item.downloads}
-//         </p>
-//       </div>
-//     </a>`;
-//     })
-//     .join('');
-
-//   gallery.insertAdjacentHTML('beforeend', markup);
-
-//   lightboxGallery.refresh();
-// }
-
 async function scrolling() {
+  console.log(page);
   page += 1;
+  console.log(page);
 
   //Checking for maximum pages number
 
@@ -178,66 +128,3 @@ async function scrolling() {
     window.removeEventListener('scroll', infiniteScrollHandler);
   }
 }
-
-// async function scrolling() {
-//   page += 1;
-
-//   //Checking for maximum pages number
-//   if (page === totalPages) {
-//     Notiflix.Notify.warning(
-//       'Sorry, these are the last images matching your search query. Please try to search something else.'
-//     );
-//     spinner.classList.remove('visually-hidden');
-
-//     try {
-//       const data = await fetchImages(currentQuery, page);
-
-//       if (data.hits.length !== 0) {
-//         renderGallery(data.hits);
-//         // Notiflix.Notify.success(`Hooray! We found ${data.hits.length} images.`);
-//         window.scrollBy({
-//           top: 200 * 2,
-//           behavior: 'smooth',
-//         });
-//         spinner.classList.add('visually-hidden');
-//         window.removeEventListener('scroll', infiniteScrollHandler);
-//         return;
-//       }
-//     }
-//     catch (error) {
-//       //fires in case of error
-
-//       console.log(error);
-
-//       spinner.classList.add('visually-hidden');
-
-//       window.removeEventListener('scroll', infiniteScrollHandler);
-//     }
-
-//     return;
-//   }
-
-//   spinner.classList.remove('visually-hidden');
-//   window.removeEventListener('scroll', infiniteScrollHandler);
-
-//   //fires if fetch results in non-empty array
-//   try {
-//     const data = await fetchImages(currentQuery, page);
-//     if (data.hits.length !== 0) {
-//       renderGallery(data.hits);
-//       // Notiflix.Notify.success(`Hooray! We found ${data.hits.length} images.`);
-//       window.scrollBy({
-//         top: 200 * 2,
-//         behavior: 'smooth',
-//       });
-//       spinner.classList.add('visually-hidden');
-//       window.addEventListener('scroll', infiniteScrollHandler);
-//       return;
-//     }
-//   } catch (error) {
-//     //fires in case of error
-//     console.log(error);
-//     spinner.classList.add('visually-hidden');
-//     window.removeEventListener('scroll', infiniteScrollHandler);
-//   }
-// }
